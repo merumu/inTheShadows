27606c06af597ac07c24814e9920a2b4fc6634a6
Martin LEROY <maleroy@student.42.fr>
2019-02-04 16:15:06 +0100

    drag n rotate

---
+++ inTheShadows/Assets/Scripts/DragnRotate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DragnRotate : MonoBehaviour{
+
+	private Vector3 positionTmp = Vector3.zero;
+
+	void OnMouseDrag(){
+		if (!Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftControl))
+		{
+			if (positionTmp.x < Input.mousePosition.x)
+				transform.rotation *= Quaternion.Euler(0,0,6);
+			if (positionTmp.x > Input.mousePosition.x)
+				transform.rotation *= Quaternion.Euler(0,0,-6);
+		}
+		if (Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftControl))
+		{
+			if (positionTmp.x < Input.mousePosition.x)
+				transform.rotation *= Quaternion.Euler(-6,0,0);
+			if (positionTmp.x > Input.mousePosition.x)
+				transform.rotation *= Quaternion.Euler(6,0,0);
+		}
+		if (!Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.LeftControl))
+		{
+			if (positionTmp.x < Input.mousePosition.x)
+				transform.rotation *= Quaternion.Euler(0,6,0);
+			if (positionTmp.x > Input.mousePosition.x)
+				transform.rotation *= Quaternion.Euler(0,-6,0);
+		}
+		positionTmp = Input.mousePosition;
+	}
+}
