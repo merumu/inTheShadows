75c9416527fbeb031c001dc33b1fe0b18e968bdf
Martin LEROY <maleroy@student.42.fr>
2019-02-07 14:56:12 +0100

    4 puzzle

--- inTheShadows/Assets/Scripts/DragnRotate.cs
+++ inTheShadows/Assets/Scripts/DragnRotate.cs
@@ -7,49 +7,53 @@ using UnityEngine.EventSystems;
 public class DragnRotate : MonoBehaviour{
 
 	public bool rotX;
-	public bool rotZ;
+	public bool rotY;
 	public bool moveY;
 	public bool mirror;
+	public Vector3 raydir;
 	public Vector3 win;
-	private bool success = false;
+	public bool success = false;
+	public bool end = false;
 	private float timer = 0;
 	private Vector3 positionTmp = Vector3.zero;
 
 	void Update(){
-		if (!success)
+		if (!end)
 			solution();
-		if (success)
-			successPos();
 	}
 
-	private void successPos()
+	public void successPos()
 	{
 		if (timer < 0)
 			transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(win), 10f);
 		else if (timer > 0)
-			transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(new Vector3(win.x + 90, win.y, win.z)), 10f);
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(new Vector3(win.x, win.y + 180, win.z)), 10f);
+		end = true;
 		Debug.Log("Success !");
 	}
 
 	private void solution()
 	{
 		RaycastHit hit;
-		Vector3 forward = transform.TransformDirection(Vector3.down * 10000);
+		Vector3 forward = transform.TransformDirection(raydir * 10000);
 		Ray ray = new Ray(transform.position, forward);
-		Vector3 forward2 = transform.TransformDirection(Vector3.up * 10000);
+		Vector3 forward2 = transform.TransformDirection(raydir * -10000);
 		Ray ray2 = new Ray(transform.position, forward2);
-		if (Physics.Raycast(ray, out hit, 10, 1 << LayerMask.NameToLayer("solution")))
+		if (Physics.Raycast(ray, out hit, 100, 1 << LayerMask.NameToLayer("solution")) && (!moveY || Vector3.Angle(forward, Vector3.back) < 10f))
         {
           	if (hit.collider)
            		timer += Time.deltaTime;
 		}
-		else if (mirror && Physics.Raycast(ray2, out hit, 10, 1 << LayerMask.NameToLayer("solution")))
+		else if (mirror && Physics.Raycast(ray2, out hit, 100, 1 << LayerMask.NameToLayer("solution")))
 		{
 			if (hit.collider)
            		timer -= Time.deltaTime;
 		}
 		else
+		{
 			timer = 0;
+			success = false;
+		}
 		if (timer > 0.5f || timer < -0.5f)
 			success = true;
 		Debug.DrawLine (transform.position, forward, Color.red);
@@ -58,12 +62,12 @@ public class DragnRotate : MonoBehaviour{
 	}
 
 	void OnMouseDrag(){
-		if (!Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftControl) && rotZ)
+		if (!Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftControl) && rotY)
 		{
 			if (positionTmp.x < Input.mousePosition.x)
-				transform.rotation *= Quaternion.Euler(0,0,4);
+				transform.rotation *= Quaternion.Euler(0,4,0);
 			if (positionTmp.x > Input.mousePosition.x)
-				transform.rotation *= Quaternion.Euler(0,0,-4);
+				transform.rotation *= Quaternion.Euler(0,-4,0);
 		}
 		if (Input.GetKey(KeyCode.LeftShift) && !Input.GetKey(KeyCode.LeftControl) && rotX)
 		{
---
+++ inTheShadows/Assets/Scripts/success.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class success : MonoBehaviour {
+
+	public DragnRotate piece1;
+	public DragnRotate piece2;
+
+	public bool multi;
+	
+	void Update () {
+		if (!multi && piece1.success)
+			piece1.successPos();
+		if (multi && piece1.success && piece2.success)
+		{
+			piece1.successPos();
+			piece2.successPos();
+		}
+	}
+}
